bed9bbfb12eab000c78965f5d7637c6a3d4f628c
sachinsisondiya <Sachinsisondiya912@gmail.com>
2025-07-22 12:04:29 +0530

    completed

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log("sachin")
 let allCards= document.querySelectorAll(".card");
 let hasFlipedCard=false;
 let lockboard=false;
@@ -9,6 +8,8 @@ let seconds = 0;
 let timeDiv = document.getElementById('timing')
 
 let resetBtn= document.getElementById('resetButton')
+let moves=0;
+let countMove= document.getElementById('count')
 
 
 
@@ -16,22 +17,35 @@ function flipCard(){
   if(lockboard){
     return;
   }
-  this.classList.add('flip');
+  if(this === firstCard) return 
+  this.classList.add('flip')
   if(! hasFlipedCard){
-     hasFlipedCard=true;
-     firstCard=true;
-     return;
+     hasFlipedCard=true
+     firstCard=this
+     return
   }
-  secondCard=this;
-  lockboard=true;
+  secondCard=this
+  lockboard=true
+
+  
+  checkForMatch();
+}
+function move(){
+  moves++
+  countMove.innerHTML = `<p class="cnt">Moves : ${moves}</p>`
 }
 function checkForMatch(){
+  move()
+  
   let match= firstCard.dataset.framework === secondCard.dataset.framework
+ 
   if(match){
     disabledCards()
+ 
   }
   else{
     unflipCards();
+  
   }
   checkForWin()
 }
@@ -44,23 +58,23 @@ function disabledCards(){
 
 function unflipCards(){
   setTimeout(function (){
-    firstCard.classList.remove('flip');
+    firstCard.classList.remove('flip')
     secondCard.classList.remove('flip')
     resetBoard()
   },1500);
  
 }
 function resetBoard(){
-  hasFlipedCard=false;
-  lockboard=false;
-  firstCard=null;
-  secondCard=null;
+  hasFlipedCard=false
+  lockboard=false
+  firstCard=null
+  secondCard=null
 }
 function shuffleCards(){
   for(let i=0;i<allCards.length;i++){
     let suffle = Math.floor(Math.random()*16);
     allCards[i].style.order=suffle;
-    console.log(allCards[i].style.order);
+    
   } 
 }
 
@@ -76,7 +90,7 @@ function resetGame(){
    stopTimer()
    seconds = 0
    startTimer()
-   document.getElementById('resultShow').innerHTML= "";
+   document.getElementById('resultShow').innerHTML= ""
 }
 function timeFormat(time){
   return time < 10 ? `0${time}`: time;  
@@ -96,15 +110,15 @@ function stopTimer(){
   clearInterval(timeInterval);
 }
 function checkForWin(){
-  let allFlippedCrds = document.querySelecttorAll('.flip').length === allCards.length
+  let allFlippedCrds = document.querySelectorAll('.flip').length === allCards.length
   if(allFlippedCrds){
     stopTimer()
 
     const hours = Math.floor(seconds/3600)
-    const minutes = Math.floor((seconds%60)/60);
+    const minutes = Math.floor((seconds%60)/60)
     const remainingSecond= seconds %60
 
-    document.getElementById('resultShow').innerHTML= `Congratulations! you won the game in ${timeFormat(hours)}:${timeFormat(minutes)}:${timeFormat(remainingSecond)}`
+    document.getElementById('resultShow').innerHTML= `<p class="result">Congratulations! you won the game in ${timeFormat(hours)}:${timeFormat(minutes)}:${timeFormat(remainingSecond)} </p>`
 
   }
 }
@@ -120,5 +134,5 @@ function startGame(){
 }
 
 let btnStartGame =document.getElementById('startButton');
-btnStartGame.addEventListener('click',startGame);
+btnStartGame.addEventListener('click',startGame)
  
